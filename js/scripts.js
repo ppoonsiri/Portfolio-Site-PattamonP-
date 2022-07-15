95465b811ab716114fabde66e8666523c3bf1a94
ppoonsiri <108196280+ppoonsiri@users.noreply.github.com>
2022-07-15 18:19:03 +0200

    Create Hamburger Menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hello, I hope you like my portfolio site");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
