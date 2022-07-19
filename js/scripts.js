76bfe1235e45abd5cf1b985ee41cf05b6b2572c7
ppoonsiri <108196280+ppoonsiri@users.noreply.github.com>
2022-07-19 22:07:56 +0200

    Update according to user feedbacks
    - Fix misspelling
    - Delete active underline in toggle menu
    - Set toggle menu to max 640 width
    - Resize prototype
    - Fix competitor images to center align

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ console.log("Hello, I hope you like my portfolio site");
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
+  if (x.className === 'navtoggle'&& $(window).width() < 640) {
     x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
